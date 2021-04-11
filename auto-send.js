0a175e965d315ba12fbcf6ca15e055fc056c2207
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-04-11 21:40:27 +0900

    자동 파일 전송

---
+++ auto-send.js
@@ -0,0 +1,5 @@
+setInterval(function() {
+  if(document.querySelector("#app-mount>div>div>div>div>div>div>div>button>div>span") !== undefined) {
+    document.querySelector("#app-mount>div>div>div>div>div>div>div>button>div>span").click()
+  }
+}, 200);
