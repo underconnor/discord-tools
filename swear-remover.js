f28f708aea359368ba57473b6f685cf875d98178
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-04-12 21:15:54 +0900

    Update swear-remover.js

--- swear-remover.js
+++ swear-remover.js
@@ -56,7 +56,8 @@ setInterval(function() {
                                 continue;
                             }
                             for(temp_3 = 0; temp_3 < blacklisted_swears.length; temp_3++) {
-                                if(_message_content.innerHTML.indexOf("<span style=\"color: red;\" onmouseover=\"this.innerHTML = '"+blacklisted_swears[temp_3]+"';\" onmouseout=\"this.innerHTML = '삐-'\">삐-</span>") != -1) {
+                                console.log(_message_content.innerHTML.indexOf("<span style=\"color: red;\" onmouseover=\"this.innerHTML = '") != -1);
+                                if(_message_content.innerHTML.indexOf("<span style=\"color: red;\" onmouseover=\"this.innerHTML = '") != -1) {
                                     continue;
                                 }
                                 _message_content.innerHTML = _message_content.innerHTML.replace(new RegExp(blacklisted_swears[temp_3], "g"), "<span style=\"color: red;\" onmouseover=\"this.innerHTML = '"+blacklisted_swears[temp_3]+"';\" onmouseout=\"this.innerHTML = '삐-'\">삐-</span>");
