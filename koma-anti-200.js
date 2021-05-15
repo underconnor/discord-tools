7b69dc16c6a77c9b60d4602d15156f5569daa147
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-05-15 18:13:52 +0900

    Update koma-anti-200.js

--- koma-anti-200.js
+++ koma-anti-200.js
@@ -1,7 +1,7 @@
 var totallength = 0;
 var range_temp = 0;
 setInterval(function() {
-    if(document.querySelector("nav>div>header>h1").innerHTML == "코마공화국") {
+    if(document.querySelector("nav>div>header>h1") != null && document.querySelector("nav>div>header>h1").innerHTML == "코마공화국") {
         totallength = 0;
         for(range_temp = 0; range_temp < document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span").length; range_temp++) {
             totallength += document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[range_temp].innerHTML.length;
