6010ae43eebb3e4406e8e9f3ca46010814c9bad1
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-05-29 09:29:01 +0900

    Update auto-disable-reply-mention.js

--- auto-disable-reply-mention.js
+++ auto-disable-reply-mention.js
@@ -4,17 +4,17 @@ setInterval(function() {
     // console.log("user_enabled: "+user_enabled);
     // console.log("addlistener"+addlistener);
     // console.log(document.querySelectorAll("form>div>div>div>div>div>div")[1]);
-    if(!reply_ui_check() && document.querySelectorAll("form>div>div>div>div>div>div")[1].getAttribute("aria-checked") == "true" && user_enabled == false) {
-        document.querySelectorAll("form>div>div>div>div>div>div")[1].dataset.auto = true;
-        document.querySelectorAll("form>div>div>div>div>div>div")[1].click();
+    if(!reply_ui_check() && document.querySelectorAll("form>div>div>div>div>div>div>div")[1].getAttribute("aria-checked") == "true" && user_enabled == false) {
+        document.querySelectorAll("form>div>div>div>div>div>div>div")[1].dataset.auto = true;
+        document.querySelectorAll("form>div>div>div>div>div>div>div")[1].click();
     }
     else if(reply_ui_check()) {
         user_enabled = false;
     }
 
     if(!reply_ui_check() && addlistener) {
-        document.querySelectorAll("form>div>div>div>div>div>div")[1].addEventListener("click", function() {
-            var it = document.querySelectorAll("form>div>div>div>div>div>div")[1];
+        document.querySelectorAll("form>div>div>div>div>div>div>div")[1].addEventListener("click", function() {
+            var it = document.querySelectorAll("form>div>div>div>div>div>div>div")[1];
             if(it.dataset.auto !== undefined && it.dataset.auto == true) {
                 it.dataset.auto == false;
                 user_enabled = false;
@@ -32,7 +32,7 @@ setInterval(function() {
 
 function reply_ui_check() {
     try {
-        return document.querySelectorAll("form>div>div>div>div>div>div")[1].className.indexOf("buttons-") == 0 || document.querySelectorAll("form>div>div>div>div>div>div")[1].className.indexOf("attachWrapper-") == 0;
+        return document.querySelectorAll("form>div>div>div>div>div>div>div")[1].className.indexOf("buttons-") == 0 || document.querySelectorAll("form>div>div>div>div>div>div>div")[1].className.indexOf("attachWrapper-") == 0;
     }
     catch {
         return true;
