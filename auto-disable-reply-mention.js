b9a84a809ce3457250c8a0ea5639fc1ddc3b723e
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-06-04 16:07:18 +0900

    Update auto-disable-reply-mention.js

--- auto-disable-reply-mention.js
+++ auto-disable-reply-mention.js
@@ -1,20 +1,19 @@
 var user_enabled = false;
 var addlistener = true;
 setInterval(function() {
-    var mention_button = document.querySelectorAll("form>div>div>div>div>div>div>div")[1];
-    var mention_button_div = document.querySelectorAll("form>div>div>div>div>div>div")[1];
-    if(!reply_ui_check() && mention_button.getAttribute("aria-checked") == "true" && user_enabled == false) {
-        mention_button_div.dataset.auto = true;
-        mention_button.click();
+
+    if(!reply_ui_check() && document.querySelectorAll("form>div>div>div>div>div>div>div>div")[1].getAttribute("aria-checked") == "true" && user_enabled == false) {
+        document.querySelectorAll("form>div>div>div>div>div>div>div")[1].dataset.auto = true;
+        document.querySelectorAll("form>div>div>div>div>div>div>div>div")[1].click();
     }
     else if(reply_ui_check()) {
         user_enabled = false;
     }
 
     if(!reply_ui_check() && addlistener) {
-        mention_button_div.addEventListener("click", function() {
-            if(mention_button_div.dataset.auto !== undefined && mention_button_div.dataset.auto == true) {
-                mention_button_div.dataset.auto == false;
+        document.querySelectorAll("form>div>div>div>div>div>div>div")[1].addEventListener("click", function() {
+            if(document.querySelectorAll("form>div>div>div>div>div>div>div")[1].dataset.auto !== undefined && document.querySelectorAll("form>div>div>div>div>div>div")[1].dataset.auto == true) {
+                document.querySelectorAll("form>div>div>div>div>div>div>div")[1].dataset.auto == false;
                 user_enabled = false;
             }
             else {
