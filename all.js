b21cb82b52b9e11bb7bbf447c24bba306d1cd865
fsanchir <64943969+fsanchir@users.noreply.github.com>
2021-09-25 21:14:50 +0900

    Update all.js

--- all.js
+++ all.js
@@ -1,23 +1,16 @@
 // 한번에 작동시키는 코드로, 업데이트가 늦을 수 있으며, 버그 또한 존재할 수 있습니다.
-var divs = 0;
-var message_div = null;
-var gotdiv = null;
 var dont_remove = false;
+var msg_scan_amount = 30;
 var remove_contents_only = false;
-var totallength = 0;
 var user_enabled = false;
 var addlistener = true;
-var range_temp = 0;
-var _message = null;
 var button = "form>div>div>div>div>div>div>div>div>div>div";
 var b_div = "form>div>div>div>div>div>div>div>div>div";
-var msg_scan_amount = 10;
-var interval_is_running = false;
-var blacklisted_swears = ["씨발", "개새끼", "병신", "ㅄ", "ㅂㅅ", "좆", "ㅆㅂ", "ㅅㅂ", "욕설로 분류되는 욕설이 아닌 매우 착한 테스트용 텍스트"]
-
+var totallength = 0;
+var range_temp = 0;
 try {
     try {
-            if(document.querySelector("#app-mount>div>div>div>div>div>div>div>div>section>div>div>div>div").innerHTML == "FSanchir") {
+        if(document.querySelectorAll("#app-mount>div>div>div>div>div>div>div>div>section")[0].childNodes[2].childNodes[1].childNodes[0].innerText == "FSanchir") {
             remove_contents_only = true;
         }
     }
@@ -32,33 +25,15 @@ try {
 }
 catch {} // 쓸 데 없지만 뭔가 추가 예정이에요
 
-for(divs = 0; divs < document.querySelectorAll("div").length; divs++) {
-    if(document.querySelectorAll("div")[divs].getAttribute("aria-label") == null) {
-        continue;
-    }
-    if(document.querySelectorAll("div")[divs].getAttribute("aria-label").indexOf("의 메시지") != -1) {
-        message_div = document.querySelectorAll("div")[divs];
-        break;
-    }
-}
+message_ol = document.querySelectorAll("ol")[0];
 
 setInterval(function() {
     try {
-        message_div.getAttribute("aria-label").indexOf("의 메세지")   
+        var msg = document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[0].innerHTML;
     }
     catch {
-        for(divs = 0; divs < document.querySelectorAll("div").length; divs++) {
-            if(document.querySelectorAll("div")[divs].getAttribute("aria-label") == null) {
-                continue;
-            }
-            if(document.querySelectorAll("div")[divs].getAttribute("aria-label").indexOf("의 메시지") != -1) {
-                message_div = document.querySelectorAll("div")[divs];
-                break;
-            }
-        }
+        return;
     }
-
-    var msg = document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[0].innerHTML;
     msg = msg.replace("<br>", "");
     try {
         if(msg.indexOf(",dontremove ") == 0) {
@@ -69,7 +44,7 @@ setInterval(function() {
             if(msg == "false") {
                 dont_remove = false;
             }
-        }
+        }   
     }
     catch {}
     try {
@@ -84,13 +59,15 @@ setInterval(function() {
         }
     }
     catch {}
-
-    // console.log(message_div);
-    for(divs = document.querySelectorAll("div."+message_div.className+">div").length - msg_scan_amount+1; divs < document.querySelectorAll("div."+message_div.className+">div").length; divs++) {
-        // console.log("div."+message_div.className+">div");
-        // console.log(document.querySelectorAll("div."+message_div.className+">div"));
-        // console.log(document.querySelectorAll("div."+message_div.className+">div").length);
-        gotdiv = document.querySelectorAll("div."+message_div.className+">div")[divs];
+    try {
+        message_ol.getAttribute("aria-label").indexOf("의 메세지")
+    }
+    catch {
+        message_ol = document.querySelectorAll("ol")[0]
+    }
+     // for(divs = document.querySelectorAll("ol."+message_ol.className+">div").length - msg_scan_amount+1; divs < document.querySelectorAll("ol."+message_ol.className+">div").length; divs++) {
+    for(divs = 0; divs < document.querySelectorAll("ol."+message_ol.className+">div").length; divs++) {
+        gotdiv = document.querySelectorAll("ol."+message_ol.className+">div")[divs];
         if(gotdiv === undefined) {
             console.log("gotdiv is undefined. continue");
             continue;
@@ -109,77 +86,24 @@ setInterval(function() {
                     if(remove_contents_only) {
                         outerhtml = "<div style='text-align: center;'><br><br><span style='color: red; font-size: large; text-align: center; font-weight: bold; margin: 1em; border: 7px solid black;'>차단된 메세지</span></div><br><br>";
                     }
-                    document.querySelectorAll("div."+message_div.className+">div")[divs].outerHTML = outerhtml;
+                    console.log("Removed Chat!");
+                    document.querySelectorAll("ol."+message_ol.className+">div")[divs].outerHTML = outerhtml;
                 }
             }
         }
         catch (error){
             console.log(error);
         }
-        if(gotdiv.className.indexOf("message-") != -1 && gotdiv.id.indexOf("chat-messages-") != -1) {
-            _message = gotdiv.childNodes;
-            for(temp_1 = 0; temp_1 < _message.length; temp_1++) {
-                if(_message[temp_1] === undefined) {
-                    console.log("undefined. continue");
-                    continue;
-                }
-                if(_message[temp_1].outerHTML.indexOf("<div ") == -1) {
-                    console.log("it's not div. continue");
-                    continue;
-                }
-                
-                if(_message[temp_1].className.indexOf("contents-") != -1 && _message[temp_1].getAttribute("role") == "document") {
-                    _childnodes = _message[temp_1].childNodes;
-                    for(temp_2 = 0; temp_2 < _childnodes.length; temp_2++) {
-                        if(_childnodes[temp_2].className.indexOf("markup-") != -1 && _childnodes[temp_2].className.indexOf("messageContent-")) {
-                            _message_content = _childnodes[temp_2];
-                            if(_message_content === null) {
-                                continue;
-                            }
-                            for(temp_3 = 0; temp_3 < blacklisted_swears.length; temp_3++) {
-                                if(_message_content.dataset.modified !== undefined && _message_content.dataset.modified) {
-                                    continue;
-                                }
-                                nowcont = _message_content.innerHTML;
-                                content = "<span style='color: red;'>삐-</span>";
-                                // content = "<span style='color: red;' onmouseover='this.innerHTML = \""+blacklisted_swears[temp_3]+"\";' onmouseout='this.innerHTML = \"삐-\"'>삐-</span>";
-                                if(_message_content.innerHTML.indexOf(blacklisted_swears[temp_3]) != -1) {
-                                    _message_content.innerHTML = _message_content.innerHTML.replace(new RegExp(blacklisted_swears[temp_3], "g"), content);
-                                }
-                                if(_message_content.innerHTML != nowcont) {
-                                    _message_content.dataset.modified = true;
-                                }
-                            }
-                            // console.log(_message_content.innerHTML);
-                            
-                            // console.log(_message_content);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
     }
-    if(document.querySelector("nav>div>header>h1") != null && document.querySelector("nav>div>header>h1").innerHTML == "코마공화국") {
-        totallength = 0;
-        for(range_temp = 0; range_temp < document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span").length; range_temp++) {
-            totallength += document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[range_temp].innerHTML.length;
-        }
-        if(totallength > 199) {
-            for(range_temp = 0; range_temp < document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span").length; range_temp++) {
-                document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[range_temp].style = "color: red;";
-            }
-        }
-        else {
-            if(document.querySelector("main>form>div>div>div>div>div>div>div>div>span>span>span").style == "color: red;") {
-                document.querySelector("main>form>div>div>div>div>div>div>div>div>span>span>span").style = "color: white;";
-            }
-        }
-    }
+}, 200);
+
+setInterval(function() {
+
 
     if(!reply_ui_check() && document.querySelectorAll(button)[1].getAttribute("aria-checked") == "true" && user_enabled == false) {
         document.querySelectorAll(b_div)[1].dataset.auto = true;
         document.querySelectorAll(button)[1].click();
+        console.log("Auto-Disabled Reply Mention!");
     }
     else if(reply_ui_check()) {
         user_enabled = false;
@@ -200,10 +124,6 @@ setInterval(function() {
     else if(reply_ui_check()){
         addlistener = true;
     }
-
-  if(document.querySelector("#app-mount>div>div>div>div>div>div>div>button>div>span") != null) {
-    document.querySelector("#app-mount>div>div>div>div>div>div>div>button>div>span").click()
-  }
 }, 200);
 
 function reply_ui_check() {
@@ -216,3 +136,28 @@ function reply_ui_check() {
     }
     
 }
+setInterval(function() {
+    if(document.querySelector("nav>div>header>h1") != null && document.querySelector("nav>div>header>h1").innerHTML == "코마공화국") {
+        totallength = 0;
+        for(range_temp = 0; range_temp < document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span").length; range_temp++) {
+            totallength += document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[range_temp].innerHTML.length;
+        }
+        if(totallength > 199) {
+            for(range_temp = 0; range_temp < document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span").length; range_temp++) {
+                document.querySelectorAll("main>form>div>div>div>div>div>div>div>div>span>span>span")[range_temp].style = "color: red;";
+            }
+        }
+        else {
+            if(document.querySelector("main>form>div>div>div>div>div>div>div>div>span>span>span").style == "color: red;") {
+                document.querySelector("main>form>div>div>div>div>div>div>div>div>span>span>span").style = "color: white;";
+            }
+        }
+    }
+}, 200);
+setInterval(function() {
+    if(document.querySelector("#app-mount>div>div>div>div>div>div>div>button>div>span") != null) {
+      document.querySelector("#app-mount>div>div>div>div>div>div>div>button>div>span").click()
+      console.log("Attachment Sent!");
+    }
+  }, 200);
+  
